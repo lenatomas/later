fc9ceb582728f75edbf9581e4f53da5fbb8cd4ac
Rick Carlino <rick.carlino@gmail.com>
2017-02-16 10:42:40 -0600

    Remove line causing webpack build errors.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var globals = ["document", "window", "later"],
-    globalValues = {};
+  globalValues = {};
 
-globals.forEach(function(g) {
+globals.forEach(function (g) {
   if (g in global) globalValues[g] = global[g];
 });
 
-require(process.env['LATER_COV'] ? "./later-cov" : "./later");
+require("./later");
 
 module.exports = later;
 
-globals.forEach(function(g) {
+globals.forEach(function (g) {
   if (g in globalValues) global[g] = globalValues[g];
   else delete global[g];
 });
